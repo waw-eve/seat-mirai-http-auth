a6f4c446b8e339c8530d78048d2b615795d7fc5b
KagurazakaNyaa <i@kagurazakanyaa.com>
2021-03-31 18:49:03 +0800

    修改元数据

---
+++ src/MiraiConnectorServiceProvider.php
@@ -0,0 +1,140 @@
+<?php
+
+/**
+ * This file is part of SeAT Mirai Connector.
+ *
+ * Copyright (C) 2021  Kagurazaka Nyaa <developer@waw-eve.com>
+ *
+ * SeAT Mirai Connector  is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * any later version.
+ *
+ * SeAT Mirai Connector is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+namespace Warlof\Seat\Connector\Drivers\Mirai;
+
+use Seat\Services\AbstractSeatPlugin;
+
+/**
+ * Class MiraiConnectorServiceProvider.
+ *
+ * @package Warlof\Seat\Connector\Drivers\Mirai
+ */
+class MiraiConnectorServiceProvider extends AbstractSeatPlugin
+{
+    /**
+     * Bootstrap the application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        $this->addRoutes();
+        $this->addViews();
+        $this->addTranslations();
+    }
+
+    /**
+     * Register the application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        $this->mergeConfigFrom(
+            __DIR__ . '/Config/mirai.config.php',
+            'mirai.config'
+        );
+
+        $this->mergeConfigFrom(
+            __DIR__ . '/Config/seat-connector.config.php',
+            'seat-connector.drivers.mirai'
+        );
+    }
+
+    private function addRoutes()
+    {
+        if (!$this->app->routesAreCached()) {
+            include __DIR__ . '/Http/routes.php';
+        }
+    }
+
+    private function addViews()
+    {
+        $this->loadViewsFrom(__DIR__ . '/resources/views', 'seat-connector-mirai');
+    }
+
+    private function addTranslations()
+    {
+        $this->loadTranslationsFrom(__DIR__ . '/resources/lang', 'seat-connector-mirai');
+    }
+
+    /**
+     * Return an URI to a CHANGELOG.md file or an API path which will be providing changelog history.
+     *
+     * @example https://raw.githubusercontent.com/eveseat/seat/master/LICENSE
+     * @exemple https://api.github.com/repos/eveseat/web/releases
+     *
+     * @return string|null
+     */
+    public function getChangelogUri(): ?string
+    {
+        return 'https://raw.githubusercontent.com/waw-eve/seat-mirai-http-auth/master/CHANGELOG.md';
+    }
+
+    /**
+     * Return the plugin public name as it should be displayed into settings.
+     *
+     * @example SeAT Web
+     *
+     * @return string
+     */
+    public function getName(): string
+    {
+        return 'Mirai Connector';
+    }
+
+    /**
+     * Return the plugin repository address.
+     *
+     * @example https://github.com/eveseat/web
+     *
+     * @return string
+     */
+    public function getPackageRepositoryUrl(): string
+    {
+        return 'https://github.com/waw-eve/seat-mirai-http-auth';
+    }
+
+    /**
+     * Return the plugin technical name as published on package manager.
+     *
+     * @example web
+     *
+     * @return string
+     */
+    public function getPackagistPackageName(): string
+    {
+        return 'seat-mirai-http-auth';
+    }
+
+    /**
+     * Return the plugin vendor tag as published on package manager.
+     *
+     * @example eveseat
+     *
+     * @return string
+     */
+    public function getPackagistVendorName(): string
+    {
+        return 'KagurazakaNyaa';
+    }
+}
